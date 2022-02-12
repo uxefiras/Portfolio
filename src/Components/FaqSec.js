a3d44618020db98cee8edd895b6d7a4bb0ce7382
Firas Faraj <70647202+uxfiras@users.noreply.github.com>
2022-02-12 20:18:33 +0200

    FaqAnimation and header

--- src/Components/FaqSec.js
+++ src/Components/FaqSec.js
@@ -1,68 +1,75 @@
 import styled from "styled-components";
-import { Heromain, Description, Img, Hide } from "../style.js";
+import Toggle from "./Toggle";
+import { Heromain } from "../style.js";
 function FaqSection() {
   return (
-    <FaqStylation>
-      {/*1st question*/}
-      <div className="question">
-        <h1>Question</h1>
-        <p className="answer">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam aliquam
-          animi hic sequi corporis aliquid quisquam esse placeat adipisci,
-          corrupti ab perspiciatis. Inventore libero distinctio sunt, obcaecati
-          blanditiis eos tenetur.
-        </p>
-        <hr />
-      </div>
-      {/*2nd question*/}
-      <div className="question">
-        <h1>Question</h1>
-        <p className="answer">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam aliquam
-          animi hic sequi corporis aliquid quisquam esse placeat adipisci,
-          corrupti ab perspiciatis. Inventore libero distinctio sunt, obcaecati
-          blanditiis eos tenetur.
-        </p>
-        <hr />
-      </div>
-      {/*3rd question*/}
-      <div className="question">
-        <h1>Question</h1>
-        <p className="answer">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam aliquam
-          animi hic sequi corporis aliquid quisquam esse placeat adipisci,
-          corrupti ab perspiciatis. Inventore libero distinctio sunt, obcaecati
-          blanditiis eos tenetur.
-        </p>
-        <hr />
-      </div>
-      {/*4th question*/}
-      <div className="question">
-        <h1>Question</h1>
-        <p className="answer">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam aliquam
-          animi hic sequi corporis aliquid quisquam esse placeat adipisci,
-          corrupti ab perspiciatis. Inventore libero distinctio sunt, obcaecati
-          blanditiis eos tenetur.
-        </p>
-      </div>
-    </FaqStylation>
+    <>
+      <FaqStylation>
+        {/*1st question*/}
+        <h1>
+          FAQ <span>Questions</span>
+        </h1>
+        <QuestionDiv>
+          <Toggle tittle="Who am i?">
+            <p className="answer">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+              aliquam animi hic sequi corporis aliquid quisquam esse placeat
+              adipisci, corrupti ab perspiciatis. Inventore libero distinctio
+              sunt, obcaecati blanditiis eos tenetur.
+            </p>
+          </Toggle>
+        </QuestionDiv>
+
+        {/*2nd question*/}
+        <QuestionDiv>
+          <Toggle tittle="What is UX process?">
+            <p className="answer">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+              aliquam animi hic sequi corporis aliquid quisquam esse placeat
+              adipisci, corrupti ab perspiciatis. Inventore libero distinctio
+              sunt, obcaecati blanditiis eos tenetur.
+            </p>
+          </Toggle>
+        </QuestionDiv>
+
+        {/*3rd question*/}
+        <QuestionDiv>
+          <Toggle tittle="How you can guarneetd your work to done?">
+            <p className="answer">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+              aliquam animi hic sequi corporis aliquid quisquam esse placeat
+              adipisci, corrupti ab perspiciatis. Inventore libero distinctio
+              sunt, obcaecati blanditiis eos tenetur.
+            </p>
+          </Toggle>
+        </QuestionDiv>
+
+        {/*4th question*/}
+        <QuestionDiv>
+          <Toggle tittle="How you can contact us?">
+            <p className="answer">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+              aliquam animi hic sequi corporis aliquid quisquam esse placeat
+              adipisci, corrupti ab perspiciatis. Inventore libero distinctio
+              sunt, obcaecati blanditiis eos tenetur.
+            </p>
+          </Toggle>
+        </QuestionDiv>
+      </FaqStylation>
+    </>
   );
 }
 
+const QuestionDiv = styled.div`
+  width: 100%;
+  cursor: pointer;
+`;
 const FaqStylation = styled(Heromain)`
   flex-direction: column;
   flex-basis: 100%;
+  justify-content: space-arou;
+
   align-items: flex-start;
-  .question {
-    margin-top: 2rem;
-    .faq-line {
-      width: 100%;
-      high: 0.2rem;
-      margin: 2rem 0;
-      background-color: #f2f2f2;
-    }
-  }
 `;
 
 export default FaqSection;
