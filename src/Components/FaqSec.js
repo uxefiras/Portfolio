f23b256b4a7c2ba9817d5d4eea218bcdb97f2df9
Firas Faraj <70647202+uxfiras@users.noreply.github.com>
2022-02-13 21:15:03 +0200

    More Animation

--- src/Components/FaqSec.js
+++ src/Components/FaqSec.js
@@ -7,67 +7,65 @@ import { fade } from "../PageAnimation";
 function FaqSection() {
   const [ref, controls] = useScroll();
   return (
-    <>
-      <FaqStylation
-        ref={ref}
-        animate={controls}
-        variants={fade}
-        initial="initial"
-      >
-        {/*1st question*/}
-        <h1>
-          FAQ <span>Questions</span>
-        </h1>
-        <AnimateSharedLayout>
-          <QuestionDiv>
-            <Toggle tittle="Who am i?">
-              <p className="answer">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-                aliquam animi hic sequi corporis aliquid quisquam esse placeat
-                adipisci, corrupti ab perspiciatis. Inventore libero distinctio
-                sunt, obcaecati blanditiis eos tenetur.
-              </p>
-            </Toggle>
-          </QuestionDiv>
+    <FaqStylation
+      ref={ref}
+      animate={controls}
+      variants={fade}
+      initial="initial"
+    >
+      {/*1st question*/}
+      <h1>
+        FAQ <span>Questions</span>
+      </h1>
+      <AnimateSharedLayout>
+        <QuestionDiv>
+          <Toggle tittle="Who am i?">
+            <p className="answer">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+              aliquam animi hic sequi corporis aliquid quisquam esse placeat
+              adipisci, corrupti ab perspiciatis. Inventore libero distinctio
+              sunt, obcaecati blanditiis eos tenetur.
+            </p>
+          </Toggle>
+        </QuestionDiv>
 
-          {/*2nd question*/}
-          <QuestionDiv>
-            <Toggle tittle="What is UX process?">
-              <p className="answer">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-                aliquam animi hic sequi corporis aliquid quisquam esse placeat
-                adipisci, corrupti ab perspiciatis. Inventore libero distinctio
-                sunt, obcaecati blanditiis eos tenetur.
-              </p>
-            </Toggle>
-          </QuestionDiv>
+        {/*2nd question*/}
+        <QuestionDiv>
+          <Toggle tittle="What is UX process?">
+            <p className="answer">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+              aliquam animi hic sequi corporis aliquid quisquam esse placeat
+              adipisci, corrupti ab perspiciatis. Inventore libero distinctio
+              sunt, obcaecati blanditiis eos tenetur.
+            </p>
+          </Toggle>
+        </QuestionDiv>
 
-          {/*3rd question*/}
-          <QuestionDiv>
-            <Toggle tittle="How you can guarneetd your work to done?">
-              <p className="answer">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-                aliquam animi hic sequi corporis aliquid quisquam esse placeat
-                adipisci, corrupti ab perspiciatis. Inventore libero distinctio
-                sunt, obcaecati blanditiis eos tenetur.
-              </p>
-            </Toggle>
-          </QuestionDiv>
+        {/*3rd question*/}
+        <QuestionDiv>
+          <Toggle tittle="How you can guarneetd your work to done?">
+            <p className="answer">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+              aliquam animi hic sequi corporis aliquid quisquam esse placeat
+              adipisci, corrupti ab perspiciatis. Inventore libero distinctio
+              sunt, obcaecati blanditiis eos tenetur.
+            </p>
+          </Toggle>
+        </QuestionDiv>
 
-          {/*4th question*/}
-          <QuestionDiv>
-            <Toggle tittle="How you can contact us?">
-              <p className="answer">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
-                aliquam animi hic sequi corporis aliquid quisquam esse placeat
-                adipisci, corrupti ab perspiciatis. Inventore libero distinctio
-                sunt, obcaecati blanditiis eos tenetur.
-              </p>
-            </Toggle>
-          </QuestionDiv>
-        </AnimateSharedLayout>
-      </FaqStylation>
-    </>
+        {/*4th question*/}
+        <QuestionDiv>
+          <Toggle tittle="How you can contact us?">
+            <p className="answer">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+              aliquam animi hic sequi corporis aliquid quisquam esse placeat
+              adipisci, corrupti ab perspiciatis. Inventore libero distinctio
+              sunt, obcaecati blanditiis eos tenetur.
+            </p>
+          </Toggle>
+        </QuestionDiv>
+      </AnimateSharedLayout>
+    </FaqStylation>
   );
 }
 
--- src/Components/GlobalStyled.js
+++ src/Components/GlobalStyled.js
@@ -6,6 +6,7 @@ const GlobalStyled = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+   
 }
 body{
     background: #1b1b1b;
